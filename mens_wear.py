b26863aa2ebf918aed2ec73a247681da705473fb
devamcodes <devamagrawal.codes@gmail.com>
2020-09-13 22:22:40 +0530

    user id added to assign it as primary key

--- mens_wear.py
+++ mens_wear.py
@@ -10,12 +10,14 @@ main_menu = """
 2. Purchase Trousers
 3. Purchase Shoes
 4. View Cart
+5. Exit The Program
 """
 #todo: exit program option needed.
 My_cart = [
     {'Apparel': 'Apparel Type', "Size": 'Apparel Size', "Colour": 'Apparel Colour'},
 ]
 
+user_names = []
 user_name = input("enter your name:-")
 user_name_bool = bool(user_name)
 if user_name_bool:
@@ -24,11 +26,14 @@ if user_name_bool:
 else:
     raise ValueError("Invalid Name!! Please try again...")
 
+user_names.append(user_name)
+user_id = len(user_names)
+print(f"{user_id}")
 
 user_input_menu = int(input(main_menu))
-#todo:after getting the error the loop should continue.
-while user_input_menu != 4:
-    user_input_menu = int(input(main_menu))#todo: this menu should be kept at the end to continue the program.
+
+while user_input_menu != 5:
+
     if user_input_menu == 1:
         from Apparels import apparels_function
         print(apparels_function())
@@ -41,5 +46,11 @@ while user_input_menu != 4:
     elif user_input_menu == 4:
         print(My_cart)
         print("work is still going on..")
+
     else:
         print("Invalid Input Please Try Again!!!")
+    user_input_menu = int(input(main_menu))
+else:
+    print("Exiting the program...")
+    print("Thank you...Have A Nice Day!!!")
+
