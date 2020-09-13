2b80bcda588c3534c4b7cb2a1237d562aa053eff
devamcodes <devamagrawal.codes@gmail.com>
2020-09-13 23:32:45 +0530

    Work incomplete.This program needs some changes so.

--- mens_wear.py
+++ mens_wear.py
@@ -5,6 +5,9 @@ Author:Devam A
 Description:By importing the three shop functions in this file and changing few lines a proper shop program is created
 status:ongoing.
 """
+
+from utils import database
+
 main_menu = """
 1. Purchase Apparels
 2. Purchase Trousers
@@ -28,7 +31,9 @@ else:
 
 user_names.append(user_name)
 user_id = len(user_names)
-print(f"{user_id}")
+print(f"Your customer Id is {user_id}.NOTE:- Please remember this number as this maybe needed in future")
+
+def 
 
 user_input_menu = int(input(main_menu))
 
